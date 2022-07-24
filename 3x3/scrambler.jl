33e0522144fe336c17afcf907d2a0c73ed0a49fc
Odilf <odysseas.maheras@gmail.com>
2022-07-24 21:44:11 +0200

    implemented scrambler

--- 3x3/scrambler.jl
+++ 3x3/scrambler.jl
@@ -1,5 +1,7 @@
 include("3x3.jl")
 
+using Random
+
 isedge(piece::Piece) = sum(abs.(piece.position)) == 2
 
 # Kinda ugly
@@ -13,18 +15,23 @@ function orientation(position::Vector3, normal::Vector3)
 	# Represent "loop"
 	mask = position .!= 0 
 	p = position[mask]
-	normal = normal[mask]
+	n = normal[mask]
 
 	# Get parity. If it is negative, the loop is inversed
 	parity = reduce(*, p)
 
 	# Count spaces of separation
+	if findfirst(i -> abs(i) == 1, n) |> isnothing
+		error("What $position, $normal")
+	end
 	index = findfirst(i -> abs(i) == 1, normal) - 1
 	
 	# Mod it and apply parity
 	n = length(p)
 	mod((n - index) * parity, n)
 end
+
+scramble() |> orientation
 
 function orientation(cube::Cube)
 	edges = 0
@@ -40,4 +47,77 @@ function orientation(cube::Cube)
 	end
 
 	(edges = edges, corners = corners)
+end
+
+function isoriented(cube::Cube)
+	(e, c) = orientation(cube)
+	e % 2 == 0 && c % 3 == 0
+end
+
+function randomorientation(piece::Piece, position::Vector3)
+	mask = position .!= 0
+	i = collect(1:3)[mask] |> rand
+	n = [0, 0, 0]
+	n[i] = position[i]
+	Piece(piece.position, n)
+end
+
+function randomize(input::Cube)::Cube
+	pieces = shuffle(input |> values |> collect)
+	pairs = map(zip(keys(input), pieces)) do (pos, piece)
+		pos => randomorientation(piece, pos)
+	end
+
+	Dict(pairs)
+end
+
+function twist(normal::Vector3, position::Vector3, n::Integer = 1)::Vector3
+	n -= 1
+	i = findfirst(x -> abs(x) == 1, normal)
+
+	for _ in 1:length(normal)
+		i = i % 3 + 1
+
+		if (position[i] != 0)
+			normal = [0, 0, 0]
+			normal[i] = position[i]
+
+			
+
+			return if n > 0
+				twist(SVector(normal...), position, n)
+			else
+				normal
+			end
+		end
+	end
+
+	error("Unreachable, in theory")
+end
+
+
+twist(SVector(0, 1, 0), SVector(1, 1, 0))
+
+function scramble()
+	e = randomize(cube() |> edges)
+	c = randomize(cube() |> corners)
+	(eo, _) = orientation(e)
+	(_, co) = orientation(c)
+
+	# Flip edge if orientation is incorrect
+	if eo % 2 != 0
+		pos = [1, 1, 0]
+		p = e[pos]
+		e[pos] = Piece(p.position, twist(p.normal, SVector(pos...)))
+	end
+
+	# Twist corner if orientation is incorrect
+	dif = co % 3
+	if dif != 0
+		pos = [1, 1, 1]
+		p = c[pos]
+		c[pos] = Piece(p.position, twist(p.normal, SVector(pos...), dif))
+	end
+
+	Dict(e..., c...)
 end
--- tests/scrambler.test.jl
+++ tests/scrambler.test.jl
@@ -10,3 +10,9 @@ include("../3x3/scrambler.jl")
 @test orientation(move(cube(), "R U D2 F L R3 D F' D")) == (edges = 4, corners = 9)
 @test sum([orientation(pos, piece.normal) for (pos, piece) in move(cube(), "R F2 L B2 D2 R")]) == 0
 @test sum([orientation(pos, piece.normal) for (pos, piece) in move(cube(), "U")]) == 10
+
+# Check twists
+@test twist(SVector(0, 1, 0), SVector(1, 1, 0)) == [1, 0, 0]
+
+# Check scrambler 
+@test all([isoriented(scramble()) for _ in 1:1000])
