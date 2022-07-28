be0f1aa984551b8ec510b9b8a5b4878aa7d40dd5
Odilf <odysseas.maheras@gmail.com>
2022-07-28 20:24:01 +0200

    fixed test

--- tests/hash.test.jl
+++ tests/hash.test.jl
@@ -10,4 +10,4 @@ include("../barbarosa/hash.jl")
 @test permutations_hash(@SVector[1, 2, 3, 4, 5, 6, 7, 8], max=8) == 0
 @test permutations_hash(@SVector[2, 1, 3, 4, 5, 6, 7, 8], max=8) == 1
 @test permutations_hash(@SVector[3, 1, 2, 4, 5, 6, 7, 8], max=8) == 2
-@test cube() |> hash == [0, 0, permutations_hash(@SVector[7, 8, 9, 10, 11, 12], max=12) * 2^6]
+@test cube() |> hash == [1, 1, permutations_hash(@SVector[7, 8, 9, 10, 11, 12], max=12) * 2^6 + 1]
