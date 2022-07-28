7e859c109d84bd517c25e10734d5cc9c2d3e2d03
Odilf <odysseas.maheras@gmail.com>
2022-07-28 20:21:52 +0200

    exported more things

--- 3x3/main.jl
+++ 3x3/main.jl
@@ -2,6 +2,12 @@ module Cube3x3
 
 include("3x3.jl")
 include("scrambler.jl")
-export Piece, Cube, cube, Move, move, issolved, Vector3, scramble, issolved, neighbours, corners, edges, orientation
+include("algs.jl")
+
+export Vector3, Piece, Cube, cube, 
+Move, move, 
+issolved, scramble, neighbours, 
+corners, edges, orientation, Corners, Edges, HalfEdges, HashSet, 
+Algs
 
 end
