8ea6d9474ab538f247ba6af0fcd16e60bce9f3ff
Odilf <odysseas.maheras@gmail.com>
2022-07-28 20:22:59 +0200

    implemented mus cache storage

---
+++ barbarosa/mus_cache.jl
@@ -0,0 +1,75 @@
+# MUS stands for "Moves until solved"
+
+using Base.Threads
+using StaticArrays
+
+Cache = Vector{UInt8}
+
+const extension = ".barbarosa"
+
+@enum PieceType edge corner
+
+function getpath(type::PieceType)
+	folder = "MUS_cache"
+	folder * (type == corner ? "/corners" : "/edges") * extension
+end
+
+resetcache() = savecache(zeros(UInt8, corner_permutations), zeros(UInt8, edge_permutations))
+
+function getcache()::Tuple{Cache, Cache}
+	read(getpath(corner)), read(getpath(edge))
+end
+
+corner_cache, edge_cache = getcache()
+
+assigncache() = ((corner_cache, edge_cache) = getcache())
+
+function savecache(corner_cache::Cache, edge_cache::Cache)
+	write(getpath(corner), corner_cache)
+	write(getpath(edge), edge_cache)
+end
+
+savecache() = savecache(corner_cache, edge_cache)
+
+function cacheprogress(corner_cache::Cache, edge_cache::Cache)
+	c_count = count(n -> n != 0, corner_cache)
+	c = c_count / corner_permutations * 100
+
+	e_count = count(n -> n != 0, edge_cache)
+	e = e_count / edge_permutations * 100
+	"Corners: $c% ($c_count/$corner_permutations). Edges: $e% ($e_count/$edge_permutations)"
+end
+
+cacheprogress() = cacheprogress(corner_cache, edge_cache)
+
+function generate_cache(state::Cube, moves::UInt8, threshold::Integer)::Nothing
+	if threshold <= 0
+		return
+	end
+
+	for neighbour in neighbours(state)
+		generate_cache(neighbour, UInt8(moves + 1), threshold - 1)
+	end
+
+	(c, e1, e2) = hash(state)
+
+	corner_cache[c] = moves
+	edge_cache[e1] = moves
+	edge_cache[e2] = moves
+
+	return
+end
+
+# @time generate_cache(cube(), UInt8(0), 5)
+
+# generate_cache(cube(), UInt8(0), 4)
+
+# resetcache()
+# savecache()
+# assigncache()
+# cacheprogress()
+
+# corner_cache, edge_cache = getcache()
+
+# filter(p -> p != 0, corner_cache)
+
