3b425338dda641f02b07576fe0de767574c8a83b
Odilf <odysseas.maheras@gmail.com>
2022-08-01 11:00:23 +0200

    implemented cache by depth for initial caching

--- src/MUS/cache.jl
+++ src/MUS/cache.jl
@@ -1,20 +1,16 @@
 # MUS stands for "Moves until solved"
 
-using Base.Threads
-
 const extension = ".barbarosa"
 const base_path = joinpath(@__DIR__, "cache")
 const corner_path = joinpath(base_path, "corners" * extension)
 const edge_path = joinpath(base_path, "edges" * extension)
 
-@enum PieceType edge corner
-
-struct Cache
+mutable struct Cache
 	corners::Vector{UInt8}
 	edges::Vector{UInt8}
 end
 
-Cache() = Cache(zeros(UInt8, edge_permutations), zeros(UInt8, corner_permutations))
+Cache() = Cache(zeros(UInt8, corner_permutations), zeros(UInt8, edge_permutations))
 
 function getcache()
 	caches = map(zip([corner_path, edge_path], [corner_permutations, edge_permutations])) do (path, perms)
@@ -37,7 +33,7 @@ function cacheprogress(cache::Vector{UInt8})
 end
 
 function cacheprogress(cache::Cache)
-	(cacheprogress(cache.corners), Cacheprogress(cache.edges))
+	(cacheprogress(cache.corners), cacheprogress(cache.edges))
 end
 
 function Base.show(io::IO, cache::Cache)
@@ -45,14 +41,24 @@ function Base.show(io::IO, cache::Cache)
 	print(io, "Cache \n Corners: $cc/$ct ($cp%) \n Edges: $ec/$et ($ep%)")
 end
 
-getcache()
-
 function savecache(cache::Cache, corner_path::AbstractString, edge_path::AbstractString)
 	write(corner_path, cache.corners)
 	write(edge_path, cache.edges)
+
+	@info "Saved cache $cache"
+
 	return cache
 end
 
 savecache(cache::Cache) = savecache(cache, corner_path, edge_path )
 
 resetcache(paths...) = savecache(Cache(), paths...)
+
+function cache(cache::Vector{UInt8}, index::Integer, value::Integer)::Vector{UInt8}
+	if cache[index] != 0
+		cache[index] = value
+		@info "Cached hash $index"
+	end
+
+	cache
+end
---
+++ src/MUS/generation.jl
@@ -0,0 +1,109 @@
+function cache_by_depth(cache::Vector{UInt8}, cube::HashSet, threshold::Integer, moves::Integer = 0)::Vector{UInt8}
+	if moves > threshold
+		return []
+	end
+
+	for neighbour in neighbours(cube)
+		cache_by_depth(cache, neighbour, threshold, moves + 1)
+	end
+
+	i = if cube isa Edges
+		hash(cube)[1]
+	else
+		hash(cube)
+	end
+	cache[i] = moves
+
+	cache
+end
+
+function cache_corners_by_depth(depth::Integer)
+	cache = getcache()
+	cache.corners = cache_by_depth(cache.corners, cube() |> corners, depth)
+	savecache(cache)
+end
+
+function cache_edges_by_depth(depth::Integer)
+	cache = getcache()
+	cache.edges = cache_by_depth(cache.edges, edges(cube()), depth)
+	savecache(cache)
+end
+
+
+
+function mus_corner_heuristic(cache::Vector{UInt8}, fallback=manhattan)
+	if length(cache) != corner_permutations
+		error("Incorrect cache passed to function (it is $(length(cache)) instead of $corner_permutations")
+	end
+
+	function heuristic(cube::Corners)
+		v = cache[hash(cube)]
+		if v != 0
+			return v
+		else
+			return manhattan(cube)
+		end
+	end
+
+	return heuristic
+end
+
+function cache_corners(cache::Cache, range::AbstractRange; kwargs...)
+	heuristic = mus_corner_heuristic(cache.corners)
+
+	for i in range
+		if cache.corners[i] != 0
+			println("Skipping caching hash $i")
+		else
+			state = dehash_corners(i)
+			solution = IDAstar(state, heuristic; kwargs...)
+			cache.corners[i] = length(solution)
+			@info "Cached hash $(i)!"
+		end
+	end
+
+	@warn "REMEMBER TO SAVE CACHE!"
+
+	cache
+end
+
+
+
+
+function mus_edge_heuristic(cache::Vector{UInt8}, fallback=manhattan)
+	if length(cache) != edge_permutations
+		error("Incorrect cache passed to function (it is $(length(cache)) instead of $edge_permutations")
+	end
+
+	# function heuristic(cube::)
+	# 	v = cache[hash(cube)]
+	# 	if v != 0
+	# 		return v
+	# 	else
+	# 		return manhattan(cube)
+	# 	end
+	# end
+
+	return heuristic
+end
+
+function cache_corners(cache::Cache, range::AbstractRange; kwargs...)
+	heuristic = mus_corner_heuristic(cache.corners)
+
+	for i in range
+		if cache.corners[i] != 0
+			println("Skipping caching hash $i")
+		else
+			state = dehash_corners(i)
+			solution = IDAstar(state, heuristic; kwargs...)
+			cache.corners[i] = length(solution)
+			@info "Cached hash $(i)!"
+		end
+	end
+
+	@warn "REMEMBER TO SAVE CACHE!"
+
+	cache
+end
+
+# cache_corners(getcache(), 1:2; silent=false) |> savecache
