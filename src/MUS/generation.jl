22e846029cd6316e8c253252a4b68b236fbb2d9e
Odilf <odysseas.maheras@gmail.com>
2022-10-17 14:36:04 +0200

    removed the try catch shenanigans

--- src/MUS/generation.jl
+++ src/MUS/generation.jl
@@ -1,3 +1,14 @@
+struct StopException{T}
+	S::T
+	cache::Vector{UInt8}
+end
+
+function Base.showerror(io::IO, ex::StopException, bt; backtrace=true)
+    Base.with_output_color(get(io, :color, false) ? :green : :nothing, io) do io
+        showerror(io, ex.S)
+    end
+end
+
 function cache_by_depth(state::Cube, depth:: Integer, max_depth::Integer, cache::Vector{UInt8})
 	if depth >= max_depth
 		return cache
@@ -6,13 +17,8 @@ function cache_by_depth(state::Cube, depth:: Integer, max_depth::Integer, cache:
 	cache = cache_if_uncached_symmetry(state, cache, depth)
 
 	for connection in Cube3x3.neighbouring_moves
-		# We use try/catch in case we want to stop the program with ctrl+C
-		try
-			cache_by_depth(move(state, connection.moves), depth + connection.cost, max_depth, cache)
-		catch
-			return cache
-		end
-	end
+		cache = cache_by_depth(move(state, connection.moves), depth + connection.cost, max_depth, cache)
+	end	
 
 	return cache
 end
@@ -21,24 +27,27 @@ using Dates
 
 function cache_by_depth(max_depth::Integer, hashset::C) where C <: Cube
 	@info "Started at $(now())"
+
 	cache = cache_by_depth(hashset, 0, max_depth, getcache(C))
+
 	@info "Finished at $(now())"
 	savecache(cache, C)
 end
 
 function cache_if_uncached_symmetry(state::Cube, cache::Vector{UInt8}, value::Integer)
-	hashes::Vector{Int} = Vector{Int}(undef, 48)
-	for (i, m) in enumerate(symmetry_matrices)
-		h = hash(transform(state, m))
-		if cache[h] <= value # There is some cached symmetry, so we skip the rest.
-			return cache
-		end
-		hashes[i] = h
+	h = hash(state)
+	if value >= cache[h] # The state is cached, so we skip it
+		return cache
 	end
 	
-	for h in hashes
+	cache[h] = value 
+	for m ∈ symmetry_matrices[2:48]
+		h = hash(transform(state, m))
 		cache[h] = value
 	end
 
 	return cache
 end
+
+cache_by_depth(5, Corners())
+# cache_by_depth(5, HalfEdges())
