efcb5a9b279d383c2cd4ef786dc8611760099b96
Odilf <odysseas.maheras@gmail.com>
2023-06-16 21:33:08 +0200

    used README as crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 #![warn(missing_docs)]
-// #![warn(clippy::missing_docs_in_private_items)]
-
-//! Barbarosa is a rust library for interacting with rubik's cubes. It is
-//! designed to be fast.
-//!
-//! Currently only the 3x3x3 cube is supported.
+#![doc = include_str!("../README.md")]
 
 pub mod cube_n;
 pub mod generic;
