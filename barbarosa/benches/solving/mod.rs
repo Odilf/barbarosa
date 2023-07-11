132e926a9ec5ef3912382d002a109661784c145d
Odilf <odysseas.maheras@gmail.com>
2023-07-11 22:04:17 +0300

    mde benching nicer with macro

--- barbarosa/benches/solving/mod.rs
+++ barbarosa/benches/solving/mod.rs
@@ -1,36 +1,35 @@
 use barbarosa::{
-    cube3::heuristics,
+    cube3::heuristics::{manhattan, mus},
     cube_n::{AxisMove, Cube3},
     generic::{alg::Alg, Cube, Movable},
 };
 use criterion::Criterion;
 use rand::{rngs::StdRng, SeedableRng};
 
-pub fn bench(c: &mut Criterion) {
-    let heuristics: Vec<(&str, Box<dyn Fn(&Cube3) -> i8>)> = vec![
-        ("manhattan", Box::new(heuristics::manhattan)),
-        ("mus", Box::new(heuristics::mus)),
-    ];
+macro_rules! bench_heuristic {
+    ($heuristic:tt, $c:tt, [$($amount:literal),*]) => {
+        let mut group = $c.benchmark_group("ida*");
 
-    // Setting the rng to an arbitrary seed for reproducibility
-    let mut rng = StdRng::seed_from_u64(69420);
+        $(        
+            let mut rng = StdRng::seed_from_u64(69420);
+            let alg = Alg::<AxisMove>::random_with_rng($amount, &mut rng);
+            let cube = Cube3::new_solved().moved(&alg);
 
-    let amounts = [4, 6, 10].into_iter();
+            println!("Benching solving alg {alg}");
 
-    amounts.for_each(|move_amount: usize| {
-        let alg = Alg::<AxisMove>::random_with_rng(move_amount, &mut rng);
-        println!("alg: {}", alg);
+            let solution = cube.solve_with_heuristic(&$heuristic).unwrap();
+            assert!(cube.clone().moved(&solution).is_solved());
 
-        let cube = Cube3::new_solved().moved(&alg);
-
-        let mut group = c.benchmark_group(format!("solving/{} moves", move_amount));
-
-        for (name, heuristic) in &heuristics {
-            group.bench_function(*name, |b| {
-                b.iter(|| -> Alg<AxisMove> { cube.solve_with_heuristic(heuristic).unwrap() })
+            group.bench_function(format!("{}/{}", stringify!($heuristic), $amount), |b| {
+                b.iter(|| -> Alg<AxisMove> { cube.solve_with_heuristic(&$heuristic).unwrap() })
             });
-        }
+        )*
 
         group.finish();
-    });
+    };
+}
+
+pub fn bench(c: &mut Criterion) {
+    bench_heuristic!(manhattan, c, [4, 5]);
+    bench_heuristic!(mus, c, [4, 6, 10]);    
 }
