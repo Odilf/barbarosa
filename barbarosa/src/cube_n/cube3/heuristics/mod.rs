969a4df62011e4ebc1e6bf8e4a29d086ac57d4ea
Odilf <odysseas.maheras@gmail.com>
2023-07-29 17:16:06 +0200

    added a `zero` heuristic (as a sort of fallback)

--- barbarosa/src/cube_n/cube3/heuristics/mod.rs
+++ barbarosa/src/cube_n/cube3/heuristics/mod.rs
@@ -3,5 +3,12 @@
 pub use manhattan::manhattan;
 pub use mus::{mus, mus_with_fallback};
 
+use crate::generic::Cube;
+
 mod manhattan;
 mod mus;
+
+/// A heuristic that just always returns 0. It can be used to basically fall back to BFS.
+pub fn zero(&_: &impl Cube) -> f32 {
+    0.0
+}
