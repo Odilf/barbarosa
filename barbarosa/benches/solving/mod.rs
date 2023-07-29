aa7b25e16e31fa424434fc09aba776474aa99a2e
Odilf <odysseas.maheras@gmail.com>
2023-07-29 17:23:46 +0200

    reduced sample count on 13 moves on benchmarking

--- barbarosa/benches/solving/mod.rs
+++ barbarosa/benches/solving/mod.rs
@@ -7,26 +7,36 @@ use criterion::Criterion;
 use rand::{rngs::StdRng, SeedableRng};
 
 macro_rules! bench_heuristic {
-    ($heuristic:tt, $c:tt, [$($amount:literal),*]) => {
+    ($heuristic:tt, $c:ident, [$ ($amount:tt),* ]) => {
         let mut group = $c.benchmark_group("ida*");
 
         $(
-            let mut rng = StdRng::seed_from_u64(69420);
-            let alg = Alg::<AxisMove>::random_with_rng($amount, &mut rng);
-            let cube = Cube3::new_solved().moved(&alg);
-
-            println!("Benching solving alg {alg}");
-
-            let solution = cube.solve_with_heuristic(&$heuristic).expect("Cube should be solvable");
-            assert!(cube.clone().moved(&solution).is_solved());
-            assert!(solution.moves.len() <= alg.moves.len(), "Solution: {solution}, alg: {alg}");
-
-            group.bench_function(format!("{}/{}", stringify!($heuristic), $amount), |b| {
-                b.iter(|| -> Alg<AxisMove> { cube.solve_with_heuristic(&$heuristic).unwrap() })
-            });
+            bench_heuristic!($heuristic, $c, group, $amount);
         )*
 
         group.finish();
+    };
+
+
+
+    ($heuristic:tt, $c:tt, $group:expr, $amount:literal) => {
+        let mut rng = StdRng::seed_from_u64(69420);
+        let alg = Alg::<AxisMove>::random_with_rng($amount, &mut rng);
+        let cube = Cube3::new_solved().moved(&alg);
+
+        println!("Benching solving alg {alg}");
+
+        let solution = cube.solve_with_heuristic(&$heuristic).expect("Cube should be solvable");
+        assert!(cube.clone().moved(&solution).is_solved());
+        assert!(solution.moves.len() <= alg.moves.len(), "Solution: {solution}, alg: {alg}");
+
+        if $amount > 13 {
+            $group.sample_size(10);
+        }
+
+        $group.bench_function(format!("{}/{}", stringify!($heuristic), $amount), |b| {
+            b.iter(|| -> Alg<AxisMove> { cube.solve_with_heuristic(&$heuristic).unwrap() })
+        });
     };
 }
 
