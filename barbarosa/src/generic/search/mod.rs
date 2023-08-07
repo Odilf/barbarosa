d3346e3aaa5526b62622e8161015d8e401db536a
Odilf <odysseas.maheras@gmail.com>
2023-08-07 16:00:30 +0200

    fmt

--- barbarosa/src/generic/search/mod.rs
+++ barbarosa/src/generic/search/mod.rs
@@ -6,7 +6,7 @@ mod test;
 use super::{Alg, Cube, Movable, Move};
 
 /// A type that can solve a cube `C` using a move `M`.
-/// 
+///
 /// [`Solver`] is automatically implemented for all [`Searcher`]s
 pub trait Solver<C: Cube + Movable<M>, M: Move> {
     /// Tries to find a solution to the given cube.
@@ -14,7 +14,7 @@ pub trait Solver<C: Cube + Movable<M>, M: Move> {
 }
 
 /// A type that can search a cube `C` using a move `M`.
-/// 
+///
 /// [`Searcher`]s implement automatically [`Solver`
 pub trait Searcher<C: Cube + Movable<M>, M: Move> {
     /// Tries to find a state of `C` such that `is_target` evaluates to `true`.
